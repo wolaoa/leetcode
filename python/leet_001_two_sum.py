d8089bc7a0bc46749ab6c575f2c01e31950b1c7b
wolaoa <lishucong2011@gmail.com>
2017-11-26 12:10:02 +0800

    two sum solution with python code

---
+++ python/leet_001_two_sum.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+	# STEP 1: sort the origin list 【O(nlogn)】
+	sortedNum = sorted(nums)
+        tempL = 0
+        tempR = len(nums) - 1
+	# STEP 2: find the two facto【O(n)】
+        while tempL < tempR:
+	    total = sortedNum[tempL] + sortedNum[tempR]  
+            if total > target:
+                tempR -= 1
+            elif total < target:
+                tempL += 1
+            else:
+                break
+	if tempL == tempR:
+	    return [-1, -1]
+	# STEP 3: get index【O(n)】
+	leftIndex  = num.index(sortedNum[tempL])
+	rightIndex = num.index(sortedNum[tempR])
+	if leftIndex == rightIndex:
+	   rightIndex = nums[leftIndex + 1:].index(sortedNum[tempL]) + leftIndex + 1 
+	return min(leftIndex, rightIndex),max(leftIndex, rightIndex)
+
